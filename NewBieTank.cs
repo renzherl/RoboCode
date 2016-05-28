fdf590f6b1fe5dd37aec70263e194a9fcb26188a
Li RAO <LRao@slb.com>
2016-05-28 20:47:20 +0800

    Combine Radar targeting and bullet avoding

--- NewBieTank.cs
+++ NewBieTank.cs
@@ -12,21 +12,41 @@ namespace NewbieTank
 {
     public class NewBieTank : AdvancedRobot
     {
+        Enemy enemy = new Enemy();
         double previousEnergy = 100;
         int movementDirection = 1;
         int gunDirection = 1;
+        
 
         public override void Run()
         {
-            
+            IsAdjustGunForRobotTurn = true;
+            //IsAdjust
             SetTurnGunRight(99999);
+            while (true)
+            {
+                if (enemy.name == null)
+                {
+                    SetTurnGunRight(2 * Math.PI);
+                }
+                Execute();
+
+
+            }
             
         }
 
         // Robot event handler, when the robot sees another robot
         public override void OnScannedRobot(ScannedRobotEvent e)
         {
+            enemy.update(e, this);
+            double Offset = rectify(enemy.direction - RadarHeadingRadians);
+            //SetTurnRadarRightRadians(Offset * 1.5);
+            SetTurnGunRight(Offset * 1.5);
+            
+            Fire(2);
             //// Stay at right angles to the opponent
+            
             SetTurnRight(e.Bearing + 90 - 30 * movementDirection);
             double changeInEnergy = previousEnergy - e.Energy;
             if (changeInEnergy > 0 && changeInEnergy <= 3) 
@@ -34,10 +54,13 @@ namespace NewbieTank
          
                 movementDirection = -movementDirection;
                 SetAhead((e.Distance / 4 + 25) * movementDirection);
+                enemy.update(e, this);
+                Offset = rectify(enemy.direction - RadarHeadingRadians);
+                //SetTurnRadarRightRadians(Offset * 1.5);
+                SetTurnGunRight(Offset * 1.5);
             }
             gunDirection = -gunDirection;
-            SetTurnGunRight(99999 * gunDirection);
-            Fire(2);
+            //SetTurnGunRight(99999 * gunDirection);          
             previousEnergy = e.Energy;
         }
 
