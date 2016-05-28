e0cb32f877816d55d560a5ada6a98c60202ad73f
Li RAO <LRao@slb.com>
2016-05-28 17:12:33 +0800

    Radar locate but does not shoot

--- NewBieTank.cs
+++ NewBieTank.cs
@@ -13,46 +13,28 @@ namespace NewbieTank
     public class NewBieTank : AdvancedRobot
     {
         Enemy enemy = new Enemy();
-        private bool movingForward;
 
-        // The main method of your robot containing robot logics
         public override void Run()
         {
-            // -- Initialization of the robot --
+            
             BodyColor = (Color.Yellow);
-            GunColor = (Color.Red);
-            //IsAdjustGunForRobotTurn = true;
-            //IsAdjustRadarForGunTurn = true;
-            // Here we turn the robot to point upwards, and move the gun 90 degrees
-            TurnLeft(Heading - 90);
-            TurnGunRight(90);
-
-            // Infinite loop making sure this robot runs till the end of the battle round
+            GunColor = (Color.Black);
+            RadarColor = (Color.Green);
+            IsAdjustGunForRobotTurn = true;
+            IsAdjustRadarForGunTurn = true;
+          
             while (true)
             {
-                // Tell the game we will want to move ahead 40000 -- some large number
-                SetAhead(40000);
-                movingForward = true;
-                // Tell the game we will want to turn right 90
-                SetTurnRight(90);
-                // At this point, we have indicated to the game that *when we do something*,
-                // we will want to move ahead and turn right.  That's what "set" means.
-                // It is important to realize we have not done anything yet!
-                // In order to actually move, we'll want to call a method that
-                // takes real time, such as WaitFor.
-                // WaitFor actually starts the action -- we start moving and turning.
-                // It will not return until we have finished turning.
-                WaitFor(new TurnCompleteCondition(this));
-                // Note:  We are still moving ahead now, but the turn is complete.
-                // Now we'll turn the other way...
-                SetTurnLeft(180);
-                // ... and wait for the turn to finish ...
-                WaitFor(new TurnCompleteCondition(this));
-                // ... then the other way ...
-                SetTurnRight(180);
-                // .. and wait for that turn to finish.
-                WaitFor(new TurnCompleteCondition(this));
-                // then back to the top to do it all again
+                
+                    if (enemy.name == null)
+                    {
+                        SetTurnRadarRightRadians(2 * Math.PI);
+                        Execute();
+                    }
+                    else
+                    {
+                        Execute();
+                    }
             }
         }
 
@@ -62,7 +44,7 @@ namespace NewbieTank
             enemy.update(e, this);
             double Offset = rectify(enemy.direction - RadarHeadingRadians);
             SetTurnRadarRightRadians(Offset * 1.5);
-            smartFire(e.Distance);
+            //smartFire(e.Distance);
         }
 
         public void smartFire(double robotDistance)
